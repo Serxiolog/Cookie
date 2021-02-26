ee7078d4a3fda170fb89952579da6c10f5efc468
89176 <s.kozyrev_sk@mail.ru>
2021-02-26 13:25:51 +0400

    Cookie_with_sprite

--- pygame project/Game.py
+++ pygame project/Game.py
@@ -1,22 +1,24 @@
-import pygame, sys, math
+import pygame, sys, math, os
 
 pygame.init()
 size = width, height = 1000, 600
 screen = pygame.display.set_mode(size)
-running = False
+running = True
 cookie_per_second = 0
 score = 0
 coeff = 1
 clock = pygame.time.Clock()
 tick = 1
-MYEVENTTYPE = pygame.USEREVENT + 1
+COOKIE = pygame.USEREVENT + 1
+GAME = pygame.USEREVENT + 2
+LEAVE = pygame.USEREVENT + 3
 pause = True
 names = {0: 'Курсор', 1: 'Бабушка', 2: 'Ферма', 3: 'Шахта', 4: 'Фабрика', 5: 'Банк',
-         6: 'Храм', 7: 'Башня мага', 8: 'Ракета', 9: 'Лаборатория', 10: 'Портал', 11: 'Машина времени'}
+         6: 'Храм', 7: 'Башня мага', 8: 'Ракета', 9: 'Лаборатория', 10: 'Портал'}
 price = {0: 15, 1: 100, 2: 1100, 3: 12000, 4: 130000, 5: 1400000,
          6: 20000000, 7: 330000000, 8: 5100000000, 9: 75000000000,
-         10: 100000000000, 11: 1400000000000}
-menu_buttons = {0: 'Играть', 1: 'Смена аккаунта', 2: 'Результаты', 3: 'Выход'}
+         10: 100000000000}
+menu_buttons = {0: 'Играть', 1: 'Результаты', 2: 'Выход'}
 
 
 class Board:
@@ -36,13 +38,14 @@ class Board:
                                  x * self.cell_size + self.left, y * self.cell_size + self.top,
                                  self.cell_size, self.cell_size), 1)
                 font = pygame.font.Font(None, 30)
-                text = font.render(f'{self.board[y][0]}', True, (100, 255, 100))
-                screen.blit(text, (960, y * self.cell_size + 15))
                 text_name = font.render(f'{names[y]}', True, (100, 255, 100))
-                screen.blit(text_name, (750, y * self.cell_size + 15))
-                font = pygame.font.Font(None, 20)
-                price_n = font.render(f'{price[y]}', True, (100, 255, 100))
-                screen.blit(price_n, (650, y * self.cell_size + 15))
+                screen.blit(text_name, (820, y * self.cell_size + 15))
+                font_num = pygame.font.SysFont('Papyrus', 20)
+                text = font_num.render(f'{self.board[y][0]}', True, (100, 255, 100))
+                screen.blit(text, (960, y * self.cell_size + 15))
+                font_price = pygame.font.SysFont('Ink Free', 20)
+                price_n = font_price.render(f'{price[y]}', True, (100, 255, 100))
+                screen.blit(price_n, (600, y * self.cell_size + 15))
 
     def set_view(self, left, top, cell_size):
         self.left = left
@@ -102,7 +105,7 @@ class Board:
 
     def menu_on_click(self, cell):
         if cell:
-            print(cell)
+            self.menu_checker(cell[1])
 
     def menu_get_click(self, mouse_pos):
         cell = self.menu_get_cell(mouse_pos)
@@ -114,9 +117,9 @@ class Board:
     def menu_render(self):
         for y in range(self.height):
             for x in range(self.width):
-                pygame.draw.rect(screen, pygame.Color(255, 255, 255), (
+                '''pygame.draw.rect(screen, pygame.Color(255, 255, 255), (
                     x * self.cell_size + self.left, y * (self.cell_size // 2) + self.top,
-                    self.cell_size, self.cell_size // 2), 1)
+                    self.cell_size, self.cell_size // 2), 1)'''
                 font = pygame.font.SysFont('Comic Sans MS', 30)
                 text_name = font.render(f'{menu_buttons[y]}', True, (100, 255, 100))
                 screen.blit(text_name, (430, y * (self.cell_size // 2) + 80))
@@ -128,10 +131,18 @@ class Board:
             return None
         return cell_x, cell_y
 
+    def menu_checker(self, num):
+        if num == 0:
+            pygame.time.set_timer(GAME, 1)
+        elif num == 1:
+            pass
+        elif num == 2:
+            pygame.time.set_timer(LEAVE, 1)
 
-board = Board(1, 12)
-board.set_view(950, 0, 50)
-menu_board = Board(1, 4)
+
+board = Board(1, 11)
+board.set_view(950, 0, 55)
+menu_board = Board(1, 3)
 menu_board.set_view(400, 50, 250)
 
 
@@ -173,8 +184,42 @@ def get_click(mouse_pos):
         on_click()
 
 
+def load_image(name):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print('bad')
+    else:
+        image = pygame.image.load(fullname)
+        return image
 
-pygame.time.set_timer(MYEVENTTYPE, 1000)
+
+def print_image():
+    screen.blit(image_cursor, (750, 10))
+    screen.blit(image_grandma, (750, 60))
+    screen.blit(image_farm, (750, 110))
+    screen.blit(image_mine, (750, 160))
+    screen.blit(image_factory, (750, 210))
+    screen.blit(image_bank, (750, 260))
+    screen.blit(image_temple, (750, 310))
+    screen.blit(image_wizard, (750, 360))
+    screen.blit(image_shipment, (750, 410))
+    screen.blit(image_lab, (750, 460))
+    screen.blit(image_portal, (750, 510))
+
+
+image_bank = load_image('bank.png')
+image_cursor = load_image('cursor.png')
+image_factory = load_image('factory.png')
+image_farm = load_image('farm.png')
+image_grandma = load_image('grandma.png')
+image_lab = load_image('lab.png')
+image_mine = load_image('mine.png')
+image_portal = load_image('portal.png')
+image_shipment = load_image('shipment.png')
+image_temple = load_image('temple.png')
+image_wizard = load_image('wizard.png')
+image_background = load_image('background.png')
+pygame.time.set_timer(COOKIE, 1000)
 while pause:
     while running:
         for event in pygame.event.get():
@@ -184,12 +229,13 @@ while pause:
             if event.type == pygame.MOUSEBUTTONUP:
                 get_click(event.pos)
                 board.get_click(event.pos)
-            if event.type == MYEVENTTYPE:
+            if event.type == COOKIE:
                 score += cookie_per_second
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_g:
                     running = False
-        screen.fill(pygame.Color(77, 113, 152))
+        screen.blit(image_background, (0, 0))
+        print_image()
         board.render()
         cookie()
         scoring()
@@ -204,6 +250,11 @@ while pause:
                 running = True
         if event.type == pygame.MOUSEBUTTONUP:
             menu_board.menu_get_click(event.pos)
+        if event.type == GAME:
+            running = True
+            pygame.time.set_timer(GAME, 0)
+        if event.type == LEAVE:
+            pause = False
     screen.fill(pygame.Color('Black'))
     menu_board.menu_render()
     pygame.display.flip()
